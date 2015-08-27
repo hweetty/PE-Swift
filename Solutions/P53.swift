102b899a1cf1d0f576b399a1bceefb1e5811f4da
Jerry Yu <hweetty@gmail.com>
2015-08-26 20:05:32 -0400

    p53

---
+++ Solutions/P53.swift
@@ -0,0 +1,32 @@
+//
+// Project Euler Solutions
+// Problem 53: Combinatoric selections
+// Jerry Yu
+//
+
+import Foundation
+
+// Construct Pascal's triangle
+func p53(cutoff: Int = 1000000) {
+	var tri = Array<Int>(count: 102, repeatedValue: 0)
+	tri[0] = 1
+	var count = 0
+
+	for row in 1...100 {
+		var prev = tri[0] // Store the previous value since it will be overwritten
+		for col in 1...row {
+			let temp = prev + tri[col]
+			prev = tri[col]
+
+			if temp < cutoff {
+				tri[col] = temp
+			} else {
+				tri[col] = cutoff; // Store the cutoff value to prevent overflow (we don't need the exact value)
+				count++
+			}
+		}
+	}
+
+	println(count)	// Ans: 4075
+					// 2013 Air 0.0050s
+}
